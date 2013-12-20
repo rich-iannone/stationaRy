dc87c8ababd119c351c7033fb3917eaf075f2984
rich-iannone <riannone@me.com>
2013-12-20 01:07:47 -0800

    Modify syntax of assign statement

--- PM25.R
+++ PM25.R
@@ -61,8 +61,8 @@ year_summary_PM25 <- function(all_years = FALSE,
   # If 'single_year' specified, filter the list to only include objects of the specified year
   if (all_years == FALSE & !is.null(single_year) & is.null(year_range)) {
     assign("file_list", list.files(path = file_path, 
-                                   pattern = paste("^",single_year,"[0-9A-Z]*PM25\\.csv", sep = '')))
-    
+                                   pattern = paste("^",single_year,"[0-9A-Z]*PM25\\.csv",
+                                                   sep = '')))
   }
   
   
