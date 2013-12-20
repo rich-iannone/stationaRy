87d6faa476e22ea2885a0296b425b0cb7cbe3bae
rich-iannone <riannone@me.com>
2013-12-20 01:08:02 -0800

    Remove whitespace

--- PM25.R
+++ PM25.R
@@ -66,17 +66,6 @@ year_summary_PM25 <- function(all_years = FALSE,
   }
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   # Loop through reading in CSV files; convert time column back to POSIXct time objects
   for (i in 1:length(file_list)){
     df <- read.csv(file = paste(file_path, "/", file_list[1], sep = ''),
