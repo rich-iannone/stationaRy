7896b8349e2708f45a72eacd69a423ceb5275e99
rich-iannone <riannone@me.com>
2013-12-21 23:57:39 -0800

    Add new R script for NOx statistics

---
+++ NOx.R
@@ -0,0 +1,21 @@
+
+year_summary_NOx <- function(all_years = FALSE,
+                             single_year = NULL,
+                             year_range = NULL,
+                             file_path = NULL,
+                             quarter_bounds = c("01-01", "03-31",
+                                                 "04-01", "06-30",
+                                                 "07-01", "09-30",
+                                                 "10-01", "12-31")) {
+  
+
+  all_years <- FALSE
+  single_year <- NULL
+  year_range <- "1998-2012"
+  file_path <- "~/Documents/R (Working)"
+  quarter_bounds = c("01-01", "03-31",
+                     "04-01", "06-30",
+                     "07-01", "09-30",
+                     "10-01", "12-31")
+  
+}
