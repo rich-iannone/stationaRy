21c818e644553f7d8b044f6f49ddfe55b02b64e5
rich-iannone <riannone@me.com>
2013-12-22 22:36:02 -0800

    Modify quarter bounds to use only merged Q2 & Q3

--- year_summary_O3.R
+++ year_summary_O3.R
@@ -8,20 +8,14 @@ year_summary_O3 <- function(all_years = FALSE,
                              single_year = NULL,
                              year_range = NULL,
                              file_path = NULL,
-                             quarter_bounds = c("01-01", "03-31",
-                                                "04-01", "06-30",
-                                                "07-01", "09-30",
-                                                "10-01", "12-31")) {
+                             quarter_bounds = c("04-01", "09-30")) {
   
   
   all_years <- FALSE
   single_year <- NULL
   year_range <- "1998-2012"
   file_path <- "~/Documents/R (Working)"
-  quarter_bounds = c("01-01", "03-31",
-                     "04-01", "06-30",
-                     "07-01", "09-30",
-                     "10-01", "12-31")
+  quarter_bounds = c("04-01", "09-30")
   
   # 
   #  test:
