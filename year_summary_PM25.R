949b3b402cc691635516d9a092ce740d148bb2fa
rich-iannone <riannone@me.com>
2014-01-28 10:49:29 -0800

    Modify indentation

--- year_summary_PM25.R
+++ year_summary_PM25.R
@@ -1,11 +1,11 @@
 generate.year_summary.PM25 <- function(all_years = FALSE,
-                              single_year = NULL,
-                              year_range = NULL,
-                              file_path = NULL,
-                              quarter_bounds = c("01-01", "03-31",
-                                                 "04-01", "06-30",
-                                                 "07-01", "09-30",
-                                                 "10-01", "12-31")) {
+                                       single_year = NULL,
+                                       year_range = NULL,
+                                       file_path = NULL,
+                                       quarter_bounds = c("01-01", "03-31",
+                                                          "04-01", "06-30",
+                                                          "07-01", "09-30",
+                                                          "10-01", "12-31")) {
 
 #  Example:
 #  year_summary_PM25(single_year = 2001, file_path = "~/Documents/R (Working)")
