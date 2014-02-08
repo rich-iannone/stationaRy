1ad50599d1983d20be28424d0f6d50f9194e955e
rich-iannone <riannone@me.com>
2014-02-07 23:37:35 -0800

    Access URI stub for obtaining station metadata

--- AQDM.get.data.R
+++ AQDM.get.data.R
@@ -8,3 +8,6 @@ require(RCurl)
 # Get the start year and the end year for the data retrieval
 start_year <- 1998
 end_year <- 2012
+
+# Access URI stub for obtaining monitoring station metadata in CSV form
+URI_stub_station_metadata_CSV <- "http://www.epa.gov/airexplorer/"
