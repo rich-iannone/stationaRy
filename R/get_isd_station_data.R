c8edc001c60dca2bdedf117883e23b9cc1ad8307
rich-iannone <riannone@me.com>
2015-06-25 07:26:38 -0700

    Add linebreaks to roxygen statements

--- R/get_isd_station_data.R
+++ R/get_isd_station_data.R
@@ -1,8 +1,8 @@
 #' Get met station data from the ISD dataset
-#' @description Obtain one or more years of meteorological data for a station from the NCEI Integrated
-#' Surface Dataset (ISD).
-#' @param station_id a station identifier composed of the station's USAF and WBAN numbers, separated
-#' by a hyphen.
+#' @description Obtain one or more years of meteorological data for a station
+#' from the NCEI Integrated Surface Dataset (ISD).
+#' @param station_id a station identifier composed of the station's USAF and
+#' WBAN numbers, separated by a hyphen.
 #' @param startyear the starting year for the collected data.
 #' @param endyear the ending year for the collected data.
 #' @importFrom plyr round_any
