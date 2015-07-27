f3a6461ecf64290f34b41e2a3ac713a9d03154ba
rich-iannone <riannone@me.com>
2015-07-27 07:50:09 -0700

    Modify comment

--- tests/testthat/test-map_isd_stations.R
+++ tests/testthat/test-map_isd_stations.R
@@ -12,8 +12,7 @@ test_that("map_isd_stations returns a map object", {
                      upper_lon = -123.000) %>%
     map_isd_stations()
   
-  # Expect that the "stations_map" object inherits from
-  # "leaflet" and "htmlwidget"
+  # Expect that the "stations_map" object inherits from both "leaflet"
+  # and "htmlwidget"
   expect_is(stations_map, c("leaflet", "htmlwidget"))
-  
 })
