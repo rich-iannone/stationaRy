1fc5ee2346e1910d6f0a42c645f4774fba049007
rich-iannone <riannone@me.com>
2015-07-26 17:47:28 -0700

    Add tests for "map_isd_stations"

---
+++ tests/testthat/test-map_isd_stations.R
@@ -0,0 +1,19 @@
+context("map_isd_stations")
+
+test_that("map_isd_stations returns a map object", {
+  
+  library(magrittr)
+  
+  # Create a Leaflet map
+  stations_map <-
+    get_isd_stations(lower_lat = 49.000,
+                     upper_lat = 49.500,
+                     lower_lon = -123.500,
+                     upper_lon = -123.000) %>%
+    map_isd_stations()
+  
+  # Expect that the "stations_map" object inherits from
+  # "leaflet" and "htmlwidget"
+  expect_is(stations_map, c("leaflet", "htmlwidget"))
+  
+})
