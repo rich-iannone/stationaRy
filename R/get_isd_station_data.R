7baca186e7e041e6b5b7fd000b42d42623911de9
Richard Iannone <riannone@me.com>
2019-07-12 22:37:06 -0400

    Modify data URL (ftp -> https)

--- R/get_isd_station_data.R
+++ R/get_isd_station_data.R
@@ -211,7 +211,7 @@ get_isd_station_data <- function(station_id,
           "-", i, ".gz")
       
       try(download(
-        url = paste0("ftp://ftp.ncdc.noaa.gov/pub/data/noaa/", i,
+        url = paste0("https://www1.ncdc.noaa.gov/pub/data/noaa/", i,
                      "/", data_file_to_download),
         destfile = file.path(temp_folder, data_file_to_download)),
         silent = TRUE)
