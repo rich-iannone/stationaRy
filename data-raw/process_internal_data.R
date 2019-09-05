d152e2c478a992bef08b6fcd722be96360443c09
Richard Iannone <riannone@me.com>
2019-09-04 23:46:32 -0400

    Update process_internal_data.R

--- data-raw/process_internal_data.R
+++ data-raw/process_internal_data.R
@@ -4,7 +4,9 @@ library(usethis)
 
 history_tbl <- stationary:::get_history_tbl(perform_tz_lookup = TRUE)
 
+inventory_tbl <- stationary:::get_inventory_tbl()
+
 usethis::use_data(
-  history_tbl,
+  history_tbl, inventory_tbl,
   internal = TRUE, overwrite = TRUE
 )
