ab9b4a179b4ca1355ad6d6dbb23ed70439ce24f7
Richard Iannone <riannone@me.com>
2019-09-24 12:18:16 -0400

    Modify roxygen documentation

--- R/get_met_data.R
+++ R/get_met_data.R
@@ -16,20 +16,19 @@
 #'   This regularizes each year of data, where the number of records per year of
 #'   data will be either 8760 or 8784 (depending on whether a year is a leap
 #'   year). By default to this is `TRUE`.
-#' @param use_local_files Option to use data files already available locally.
 #' @param local_file_dir Path to local meteorological data files. If specified,
-#'   then data files will be obtained from this location and not from the remote
-#'   data store.
+#'   then data files will be downloaded to and retrieved from this location and
+#'   not from the remote data store.
 #' 
-#' @return Returns a tibble with at least 10 variables. Times are recorded 
-#' using the Universal Time Code (UTC) in the source data. Times are adjusted
-#' to local standard time for the station's locale.
+#' @return Returns a tibble with at least 10 variables. While times are recorded
+#'   using the Universal Time Code (UTC) in the source data, they are adjusted
+#'   here to local standard time for the station's locale.
 #' \describe{
 #' \item{id}{A character string identifying the fixed weather station
 #' from the USAF Master Station Catalog identifier and the WBAN identifier.}
 #' \item{time}{A datetime value representing the observation time.}
 #' \item{temp}{Air temperature measured in degrees Celsius. Conversions to
-#' degrees Farenheit may be calculated with `(temp * 9) / 5 + 32`.}
+#' degrees Fahrenheit may be calculated with `(temp * 9) / 5 + 32`.}
 #' \item{wd}{The angle of wind direction, measured in a clockwise direction,
 #' between true north and the direction from which the wind is blowing. For
 #' example, `wd = 90` indicates the wind is blowing from due east. `wd = 225`
@@ -44,7 +43,7 @@
 #' for saturation to occur.}
 #' \item{rh}{Relative humidity, measured as a percentage, as calculated using
 #' the August-Roche-Magnus approximation.}
-#' \item{ceil_hgt}{The height above ground level of the lowest clould cover or
+#' \item{ceil_hgt}{The height above ground level of the lowest cloud cover or
 #' other obscuring phenomena amounting to at least 5/8 sky coverage. Measured in
 #' meters. Unlimited height (no obstruction) is denoted by the value `22000`.}
 #' \item{visibility}{The horizontal distance at which an object can be seen and
